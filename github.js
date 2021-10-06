dfdf64eec0e395158d93a13d0356f9b22de1c32d
zach <ZCroninDev@users.noreply.github.com>
2021-10-06 12:05:12 -0400

    Github authoriziation

--- github.js
+++ github.js
@@ -1,17 +1,31 @@
 class Github {
-  contructor() {
-    this.client_id = '';
-    this.client_secret = '';
+  constructor() {
+    this.config = {
+      headers: {
+        Authorization: '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'
+      }
+    }
+    this.repos_count = 5
+    this.repos_sort = 'created: asc'
   }
-
   async getUser(user) {
-    const profileResponse = await fetch
-     (`https://api.github.com/users;${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
-     
-     const profile = await profileResponse.json();
-
-     return {
-       profile
-     }
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}`,
+      this.config
+    )
+ 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`,
+      this.config
+    )
+ 
+    const profile = await profileResponse.json()
+ 
+    const repos = await repoResponse.json()
+ 
+    return {
+      profile,
+      repos
+    }
   }
 }
